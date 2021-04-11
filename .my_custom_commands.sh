433a19dcae887bce60d347c45d19fcc7b1c79d0f
nimakos <nimakos@hotmail.com>
2021-04-11 17:50:44 +0300

    udates

--- .my_custom_commands.sh
+++ .my_custom_commands.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
 
-# download audio from youtube
+# converts and saves youtube video to mp3
 function download_audio() {
-	youtube-dl -U
-	youtube-dl -f bestaudio $1
+	# youtube-dl-f bestaudio $1
+  	youtube-dl -o "~/Music/%(title)s.%(ext)s" --extract-audio --audio-format mp3 $1
 }
 
-# download video from youtube
+# converts and saves youtube video to Video
 function download_video() {
-	youtube-dl -U
-	youtube-dl -f best $1
+	youtube-dl -o "~/Music/%(title)s.%(ext)s"  -f bestvideo+bestaudio $1
+}
+
+function make_updates() {
+	sudo ln -s /usr/bin/python3 /usr/local/bin/python
+	sudo wget https://yt-dl.org/latest/youtube-dl -O /usr/local/bin/youtube-dl
+	sudo chmod a+x /usr/local/bin/youtube-dl
+	sudo apt-get install -y ffmpeg
+	sudo youtube-dl -U	
+}
+
+# cleans the system
+function clean() {
+  sudo apt-get autoremove
+  sudo du -sh /var/cache/apt
+  sudo apt-get autoclean
+  sudo apt-get clean
+  sudo du -sh ~/.cache/thumbnails
+  sudo rm -rf ~/.cache/thumbnails/*
+}
+
+# Sound properties
+function sound() {
+  alsamixer
+  alsactl store
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Youtube
 Youtube Downloader
+
+1. Go to folder that has the sript and type : chmod +x .my_custom_commands.sh
+2. Go to root directory cd ~/
+3. type : sudo vim ~/.bashrc
+4. Go to the last line and type : /path_to_file/.my_custom_commands.sh
+5. Go t home directory and make refresh : sudo vim ~/.bashrc
+6. First run : make_updates to download the necessary packages 
